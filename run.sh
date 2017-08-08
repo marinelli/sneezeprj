1cc60eefd5081f5e1117ff91d6d534a22e48fe03
Giorgio Marinelli <giorgio@marinel.li>
2017-08-08 09:28:35 +0200

    second commit

--- run.sh
+++ run.sh
@@ -18,19 +18,33 @@ fi
 _CMD_MANAGE_PY='manage.py'
 
 if [ ! -f "$_CMD_MANAGE_PY" ] ; then
-    printf '!!! %s\n' 'manage.py not found' 1>&2
+    printf '!!! %s\n' "$_CMD_MANAGE_PY not found" 1>&2
 fi
 
 
-"$_CMD_PIP3" install --user --requirement requirements.txt || \
-    exit "$?"
+if [ ! -f 'requirements.txt' ] ; then
+    printf '!!! %s\n' 'requirements.txt not found' 1>&2
+else
+    "$_CMD_PIP3" list --format freeze > packages.txt
 
-"$_CMD_PYTHON3" manage.py makemigrations || \
-    exit "$?"
+    cat requirements.txt | \
+      while read _CUR_DEPENDENCY ; do
+	  grep -q -i -- "$_CUR_DEPENDENCY" packages.txt
 
-"$_CMD_PYTHON3" manage.py migrate || \
-    exit "$?"
+	  if [ "$?" -ne 0 ] ; then
+	      "$_CMD_PIP3" install --user "$_CUR_DEPENDENCY"
+	  fi
+      done
 
-"$_CMD_PYTHON3" manage.py runserver || \
-    exit "$?"
+    test -f packages.txt && rm packages.txt
+
+    "$_CMD_PYTHON3" manage.py makemigrations || \
+      exit "$?"
+
+    "$_CMD_PYTHON3" manage.py migrate || \
+      exit "$?"
+
+    "$_CMD_PYTHON3" manage.py runserver || \
+      exit "$?"
+fi
 
